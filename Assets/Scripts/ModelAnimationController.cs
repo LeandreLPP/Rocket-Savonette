ce9e9fc6650f4b1ce44d3e2b2d7906718b4edd55
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2023-03-10 22:19:59 +0100

    Strafe anims

--- Assets/Scripts/ModelAnimationController.cs
+++ Assets/Scripts/ModelAnimationController.cs
@@ -7,21 +7,26 @@ public class ModelAnimationController : MonoBehaviour
     public Vector2InputListener straffListener = default;
 
     private Animator animator;
-    private float straffValue = 0.5f;
-    private float turnValue = 0.5f;
+    private float straffValue;
+    private float turnValue;
+    
     public float changeTime = 0.25f;
     private float changeSpeed => 1 / changeTime;
 
+    private static readonly int _STRAFF = Animator.StringToHash("straff");
+    private static readonly int _TURN = Animator.StringToHash("turn");
+    private static readonly int _BURNING = Animator.StringToHash("burning");
+    
     private void Start() {
         animator = GetComponent<Animator>();
     }
 
     private void Update() {
-        turnValue = Mathf.MoveTowards(turnValue, (turnListener.Value.x + 1) * 0.5f, changeSpeed * Time.deltaTime);
-        straffValue = Mathf.MoveTowards(straffValue, (straffListener.Value.x + 1) * 0.5f, changeSpeed * Time.deltaTime);
+        turnValue = Mathf.MoveTowards(turnValue, turnListener.Value.x, changeSpeed * Time.deltaTime);
+        straffValue = Mathf.MoveTowards(straffValue, straffListener.Value.x, changeSpeed * Time.deltaTime);
 
-        animator.SetBool("burning", burnListener.isPressed);
-        animator.SetFloat("straff", straffValue);
-        animator.SetFloat("turn", turnValue);
+        animator.SetBool(_BURNING, burnListener.isPressed);
+        animator.SetFloat(_STRAFF, straffValue);
+        animator.SetFloat(_TURN, turnValue);
     }
 }
